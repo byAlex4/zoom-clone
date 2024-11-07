b703aeabc81afd369aae80352a3dbf6c1840f6b0
byAlex4 <alex.bocanegra.medina@gmail.com>
2024-11-07 12:19:03 -0600

    change exec build

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const io = require('socket.io')(server, {
   transports: ['websocket', 'polling']
 });
 const { v4: uuidV4 } = require('uuid');
-const { exec } = require('child_process');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -18,19 +17,6 @@ app.get('/', (req, res) => {
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room, port: PORT });
-});
-
-// Ejecutar build.js con roomId y port
-exec(`node build.js ${roomId} ${PORT}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error ejecutando build.js: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
 });
 
 io.on('connection', (socket) => {
