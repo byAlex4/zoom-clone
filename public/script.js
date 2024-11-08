0dd4fa71ae55a289aa532f7b5be5d8b37f211edc
byAlex4 <alex.bocanegra.medina@gmail.com>
2024-11-08 14:41:04 -0600

    WebSocket connection

--- public/script.js
+++ public/script.js
@@ -1,4 +1,8 @@
-const socket = io('/')
+const socket = io('/', {
+  secure: true,
+  transports: ['websocket', 'polling']
+})
+
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer(undefined, {
