2bf72162cade4def686c5831ce0a44cccac8914e
byAlex4 <alex.bocanegra.medina@gmail.com>
2024-11-12 20:23:37 -0600

    change cors

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST']
+    origin: '*',  // Usa la URL específica de tu aplicación React Native/Expo
+    methods: ['GET', 'POST'],
+    credentials: true
   }
 });
 
@@ -16,20 +17,26 @@ app.use(express.static('public'))
 
 // Configuración de rutas de Express
 app.get('/', (req, res) => {
+  console.log('Redirigiendo a una nueva sala')
   res.redirect(`/${uuidV4()}`)
 })
 
 app.get('/:room', (req, res) => {
+  console.log(`Renderizando sala con ID: ${req.params.room}`)
   res.render('room', { roomId: req.params.room })
 })
 
 // Configuración de Socket.IO
 io.on('connection', socket => {
+  console.log('Nuevo cliente conectado')
+
   socket.on('join-room', (roomId, userId) => {
+    console.log(`Usuario ${userId} se unió a la sala ${roomId}`)
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
 
     socket.on('disconnect', () => {
+      console.log(`Usuario ${userId} se desconectó de la sala ${roomId}`)
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
   })
